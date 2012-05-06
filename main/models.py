19499967a8340239d593d57f702f72db915c3cf5
Chris Cauley <chriscauley@mouthwateringmedia.com>
2012-05-06 00:13:00 -0500

    more fixes to open up to the lab

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from articles.models import Article
 
-# Create your models here.
+class OrderedModel(models.Model):
+  order = models.PositiveIntegerField(default=9999)
+  class Meta:
+    abstract = True
+
+class SlugModel(models.Model):
+  title = models.CharField(max_length=128)
+  __unicode__ = lambda self: self.title
+  slug = models.CharField(max_length=128,null=True,blank=True)
+
+  def save (self,*args,**kwargs):
+    self.slug = slugify(self.title)
+    super(SlugModel,self).save(*args,**kwargs)
+
+  class Meta:
+    abstract = True

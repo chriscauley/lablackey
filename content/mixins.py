011467d409486052f33b82b8b3fe029538e9275e
Chris Cauley <chris@lablackey.com>
2012-12-14 19:30:44 -0500

    removing ckeditor

--- content/mixins.py
+++ content/mixins.py
@@ -3,22 +3,6 @@ from django.core import urlresolvers
 from django.db import models
 from django import forms
 
-
-class CKEditorMixin(object):
-  """Changes widget for all TextFields to use CKEditor."""
-  formfield_overrides = {
-    models.TextField: {
-      'widget': forms.Textarea(attrs={'class': 'ckeditor'}),
-      },
-    }
-
-  class Media:
-    js = [
-      'js/jquery-1.7.1.min.js',
-      'ckeditor/ckeditor.js',
-      'ckeditor/config.js',
-      ]
-
 class TinyMCE(object):
   class Media:
     js = [

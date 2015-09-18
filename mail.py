d5395a5c247f5cdb55afdabac8d54fc6849ccd04
chriscauley <chris@lablackey.com>
2015-09-18 18:13:39 -0400

    ignore missing templates for html

--- mail.py
+++ mail.py
@@ -1,16 +1,21 @@
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives, send_mail
-from django.template.loader import get_template
+from django.template.loader import get_template, TemplateDoesNotExist
 from django.template import Context
 
 def send_template_email(template_name, recipients, from_email=settings.DEFAULT_FROM_EMAIL, context={}):
+  if type(recipients) in [unicode,str]:
+    recipients = [recipients]
   d = Context(context)
   msg = EmailMultiAlternatives(
-    get_template('%s_subject.html'%template_name).render(d),
+    get_template('%s.subject'%template_name).render(d).strip(), # dat trailing linebreak
     get_template('%s.txt'%template_name).render(d),
     from_email,
     recipients)
-  msg.attach_alternative(get_template('%s.html'%template_name).render(d), "text/html")
+  try:
+    msg.attach_alternative(get_template('%s.html'%template_name).render(d), "text/html")
+  except TemplateDoesNotExist:
+    pass
   msg.send()
 
 def print_to_mail(subject='Unnamed message',to=[settings.ADMINS[0][1]],notify_empty=lambda:True):

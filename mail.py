956362508842ea110739bf8470e6cbd7bfbac968
chriscauley <chris@lablackey.com>
2015-09-19 19:29:10 -0400

    adding the experimental kwarg to notify people that we are testing the auto-emailer

--- mail.py
+++ mail.py
@@ -3,13 +3,18 @@ from django.core.mail import EmailMultiAlternatives, send_mail
 from django.template.loader import get_template, TemplateDoesNotExist
 from django.template import Context
 
-def send_template_email(template_name, recipients, from_email=settings.DEFAULT_FROM_EMAIL, context={}):
+def send_template_email(template_name, recipients,
+                        from_email=settings.DEFAULT_FROM_EMAIL, context={},experimental=True):
   if type(recipients) in [unicode,str]:
     recipients = [recipients]
   d = Context(context)
+  preface = ''
+  if experimental:
+    from_email = 'chris@lablackey.com'
+    preface = "DISCLAMER: This is an automatic email with important information regarding your TXRX membership. If you believe you received this email in error, please reply and tell me why so I can correct the error.\n\n"
   msg = EmailMultiAlternatives(
     get_template('%s.subject'%template_name).render(d).strip(), # dat trailing linebreak
-    get_template('%s.txt'%template_name).render(d),
+    preface+get_template('%s.txt'%template_name).render(d),
     from_email,
     recipients)
   try:

2a73f50199be1095f14eef6427762cab6674a36c
chriscauley <chris@lablackey.com>
2015-10-23 03:18:42 -0400

    adding DebugBackend to mail

--- mail.py
+++ mail.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives, send_mail, mail_admins
+from django.core.mail.backends.smtp import EmailBackend
 from django.template.loader import get_template, TemplateDoesNotExist
 from django.template import Context
 from cStringIO import StringIO
@@ -63,3 +64,20 @@ def mail_on_fail(target):
         ]
       mail_admins("Error occurred via 'mail_on_fail'",'\n'.join(lines))
   return wrapper
+
+def filter_emails(emails):
+  if settings.DEBUG:
+    #only email certain people from dev server!
+    return [e for e in emails if e in getattr(settings,'ALLOWED_EMAILS',[])]
+
+class DebugBackend(EmailBackend):
+  def send_messages(self,email_messages):
+    if not settings.DEBUG:
+      return super(DebugBackend,self).send_messages(email_messages)
+    for message in email_messages:
+      if not settings.EMAIL_SUBJECT_PREFIX in message.subject:
+        message.subject = "%s%s"%(settings.EMAIL_SUBJECT_PREFIX,message.subject)
+      message.to = filter_emails(message.to) or [getattr(settings,'ALLOWED_EMAILS',[])[0]]
+      message.cc = filter_emails(message.cc)
+      message.bcc = filter_emails(message.bcc)
+    return super(DebugBackend,self).send_messages(email_messages)

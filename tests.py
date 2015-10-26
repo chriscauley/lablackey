6139117e31729dcc378e392bebaf5852c9e38d1e
chriscauley <chris@lablackey.com>
2015-10-26 15:51:09 -0400

    adding a way to check recipients as well

--- tests.py
+++ tests.py
@@ -1,12 +1,20 @@
 from django.conf import settings
 from django.core import mail
 
+def _check(a,b):
+  success = sorted(a) == sorted(b)
+  if not success:
+    raise ValueError("\ndesired: %s \nactual  : %s"%(sorted(a),sorted(b)))
+  return success
+
 def check_subjects(subjects,outbox=None):
   if not outbox:
     outbox = mail.outbox
   outbox_subjects = [m.subject.replace(settings.EMAIL_SUBJECT_PREFIX,'') for m in outbox]
-  success = sorted(subjects) == sorted(outbox_subjects)
-  if not success:
-    print "desired subjects: ",sorted(subjects)
-    print "outbox subjects:  ",sorted(outbox_subjects)
-  return success
+  return _check(subjects,outbox_subjects)
+
+def check_recipients(recipients,outbox=None):
+  if not outbox:
+    outbox = mail.outbox
+  outbox_recipients = [m.recipients() for m in outbox]
+  return _check(recipients,outbox_recipients)

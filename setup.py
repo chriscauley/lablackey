f1238ac7fe0972410fd6b2adb61b58050a271135
chriscauley <chris@lablackey.com>
2016-07-21 10:15:48 -0400

    submitting to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'lablackey',
+  packages = ['lablackey'], # this must be the same as the name above
+  version = '0.0.1a',
+  description = 'A collection of tools for django',
+  author = 'Chris Cauley',
+  author_email = 'chris@lablackey.com',
+  url = 'https://github.com/chriscauley/lablackey', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['utils'], # arbitrary keywords
+  classifiers = [],
+)

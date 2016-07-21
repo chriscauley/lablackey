dc713559cc5a394876298038b88eea19e578ec37
chriscauley <chris@lablackey.com>
2016-07-21 13:52:35 -0400

    think got the templates/static files now

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'lablackey',
-  packages = {'lablackey':['*']}, # this must be the same as the name above
-  version = '0.1.1',
+  packages = ['lablackey','lablackey.db'], # this must be the same as the name above
+  version = '0.1.2',
   description = 'A collection of tools for django',
   author = 'Chris Cauley',
   author_email = 'chris@lablackey.com',
   url = 'https://github.com/chriscauley/lablackey', # use the URL to the github repo
-  download_url = 'https://github.com/chriscauley/lablackey/tarball/0.1.1', # I'll explain this in a second
+  download_url = 'https://github.com/chriscauley/lablackey/tarball/0.1.2', # I'll explain this in a second
   keywords = ['utils'], # arbitrary keywords
   classifiers = [],
 )

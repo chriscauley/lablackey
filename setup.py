e2d0592bfcb698ebd6fe706312f8e3b12669b642
chriscauley <chris@lablackey.com>
2016-07-21 13:41:31 -0400

    updating version and fixing tarball location

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'lablackey',
   packages = {'lablackey':['*']}, # this must be the same as the name above
-  version = '0.1.0a',
+  version = '0.1.1',
   description = 'A collection of tools for django',
   author = 'Chris Cauley',
   author_email = 'chris@lablackey.com',
   url = 'https://github.com/chriscauley/lablackey', # use the URL to the github repo
-  download_url = 'https://github.com/chriscauley/lablackey/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/chriscauley/lablackey/tarball/0.1.1', # I'll explain this in a second
   keywords = ['utils'], # arbitrary keywords
   classifiers = [],
 )

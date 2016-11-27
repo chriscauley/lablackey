2f9a469602050b9b0dc69c66dfafe265a5fae1aa
chriscauley <chris@lablackey.com>
2016-11-27 14:01:54 -0500

    adding a few urls which will be used in many projects

--- lablackey/urls.py
+++ lablackey/urls.py
@@ -5,4 +5,8 @@ import views
 urlpatterns = [
   url(r'^api/schema/([\w\d]+).([\w\d]+)/$',views.get_schema),
   url(r'^set_email/$',views.set_email ,name='set_email'),
+  url(r'^api/login/$',views.login_ajax),
+  url(r'^api/register/$',views.register_ajax),
+  url(r'^user.json$',views.user_json),
+  url(r'^accounts/logout/$',views.logout),
 ]

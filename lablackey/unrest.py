e588974289d55059e9e47990abbccfc14a878134
chriscauley <chris@lablackey.com>
2016-11-27 14:02:15 -0500

    ManyToOneRel cannot be deconstructed, may need to come back to this

--- lablackey/unrest.py
+++ lablackey/unrest.py
@@ -1,5 +1,6 @@
-
 # This file should eventually become it's own unrest library, but then again all of lablackey might go that way
+
+from django.db import models
 
 EXCLUDE_FIELDS = ['django.db.models.AutoField']
 
@@ -12,6 +13,8 @@ def model_to_schema(model):
   schema = []
   exclude = getattr(model,"schema_exclude",[])
   for field, fk_model in model._meta.get_fields_with_model():
+    if isinstance(field,models.ManyToOneRel):
+      continue
     name, path, args, kwargs = field.deconstruct()
     if path in EXCLUDE_FIELDS or name in exclude:
       continue

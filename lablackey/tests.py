f1b75ba17fcb131960d0972036404ba5a5487c6d
chriscauley <chris@lablackey.com>
2016-12-20 14:47:03 -0500

    adding ClientTestCase

--- lablackey/tests.py
+++ lablackey/tests.py
@@ -1,5 +1,10 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core import mail
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
+
+import random
 
 def _check(a,b):
   success = sorted(a) == sorted(b)
@@ -18,3 +23,18 @@ def check_recipients(recipients,outbox=None):
     outbox = mail.outbox
   outbox_recipients = [m.recipients() for m in outbox]
   return _check(recipients,outbox_recipients)
+
+class ClientTestCase(TestCase):
+  _passwords = {}
+  def login(self,username,password=None):
+    if isinstance(username,get_user_model()):
+      username = username.username
+    self.client.post(reverse('login'),{'username': username,'password': password or self._passwords[username]})
+  def _new_object(self,model,**kwargs):
+    return model.objects.create(**kwargs)
+  def new_user(self,username=None,password=None,**kwargs):
+    user = self._new_object(get_user_model(),username=username or "user_"%random.random(),**kwargs)
+    self._passwords[user.username] = password = password or "%s"%random.random()
+    user.set_password(password)
+    user.save()
+    return user
--- lablackey/urls.py
+++ lablackey/urls.py
@@ -5,7 +5,7 @@ import views, api
 urlpatterns = [
   url(r'^api/schema/([\w\d]+).([\w\d]+)/$',views.get_schema),
   url(r'^set_email/$',views.set_email ,name='set_email'),
-  url(r'^api/login/$',views.login_ajax),
+  url(r'^api/login/$',views.login_ajax,name='login'),
   url(r'^api/register/$',views.register_ajax),
   url(r'^user.json$',views.user_json),
   url(r'^accounts/logout/$',views.logout),

adbd5099aa7ef4e94029a40076c004a2706b7638
chriscauley <chris@lablackey.com>
2017-01-12 11:28:19 -0500

    adding another keyword

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
   name = 'lablackey',
-  packages = ['lablackey','lablackey.db'], # this must be the same as the name above
+  packages = ['lablackey','lablackey.db'],
   version = '0.1.3',
   description = 'A collection of tools for django',
   author = 'Chris Cauley',
   author_email = 'chris@lablackey.com',
-  url = 'https://github.com/chriscauley/lablackey', # use the URL to the github repo
-  keywords = ['utils'], # arbitrary keywords
+  url = 'https://github.com/chriscauley/lablackey',
+  keywords = ['utils','django'],
   license = 'GPL',
   include_package_data = True,
 )

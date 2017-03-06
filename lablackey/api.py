095da40ac273ef94508e10f3590e4964e87a2e78
chriscauley <chris@lablackey.com>
2017-03-06 12:10:01 -0500

    fucking s-es

--- lablackey/api.py
+++ lablackey/api.py
@@ -8,7 +8,7 @@ def get_model(app_name,model_name):
 
 def get_one(request,app_name,model_name,pk):
   model = get_model(app_name,model_name)
-  if model.table_permsissions and not model.table_permissions(request.user):
+  if model.table_permissions and not model.table_permissions(request.user):
     raise NotImplementedError
   obj = get_object_or_404(model,pk=pk)
   if obj.row_permission and not obj.row_permissions(request.user):
@@ -17,7 +17,7 @@ def get_one(request,app_name,model_name,pk):
 
 def get_many(request,app_name,model_name):
   model = get_model(app_name,model_name)
-  if model.table_permsissions and not model.table_permissions(request.user):
+  if model.table_permissions and not model.table_permissions(request.user):
     raise NotImplementedError
   kwargs = {k: request.GET[k] for k in model.filter_fields if k in request.GET}
   objs = model.objects.filter(**kwargs)

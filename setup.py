cb06b4630c99589b10314fb1bc43c33559cf1e7d
chriscauley <chris@lablackey.com>
2018-06-22 14:52:58 +0000

    incrementing version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
   name = 'lablackey',
   packages = ['lablackey','lablackey.db'],
-  version = '0.1.3',
+  version = '0.1.5',
   description = 'A collection of tools for django',
   long_description=long_description,
   long_description_content_type="text/markdown",
